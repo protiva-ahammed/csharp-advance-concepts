ec83f356b1d680b9e8acbf81903c3a1b1d9d8ee5
ahammed-protiva <protivaahammed7137@gmail.com>
2025-08-19 10:10:54 +0900

    use of PLINQ

---
+++ WebServer/Program.cs
@@ -0,0 +1,16 @@
+﻿var items = Enumerable.Range(1, 200);
+var evenNumbers = items.AsParallel().Where(
+    x =>
+    {
+        Console.WriteLine($" Process1 num {x} ; ThreadId : {Thread.CurrentThread.ManagedThreadId} ");
+        return (x % 2 == 0);
+    }
+);
+
+Console.WriteLine();
+// ForAll() calls for run all in threads to gain parallelism
+// so no ordering and synchronization maintained
+evenNumbers.ForAll(item =>
+{
+    Console.WriteLine($" Process2 num {item} ; ThreadId : {Thread.CurrentThread.ManagedThreadId}");
+});
